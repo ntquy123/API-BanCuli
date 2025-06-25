5adcf752a295151661d20367d455e3cda12513b0
Quy <57335096+ntquy123@users.noreply.github.com>
2025-06-25 12:26:39 +0700

    fix room delete route

--- src/routes/roomRoutes.ts
+++ src/routes/roomRoutes.ts
@@ -1,12 +1,12 @@
-
+import express from 'express';
 import * as RoomController from '../controllers/roomController';
 import { spawn, exec } from 'child_process';
-const express = require('express');
+
 const router = express.Router();
 
 //router.post('/createroom/', RoomController.createRoom);
 router.get('/getroom/', RoomController.getRooms);
-router.delete('deleteroom/:roomName', RoomController.deleteRoom);
+router.delete('/deleteroom/:roomId', RoomController.deleteRoom);
 router.put('/createroom',RoomController.createRoom);
 router.post('/joinRoom',RoomController.joinRoom);
 router.post('/leaveRoom',RoomController.leaveRoom);
