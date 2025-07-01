d2c688d021548a7458dbafaf2dd5dbb5d7d67918
Quy <57335096+ntquy123@users.noreply.github.com>
2025-07-01 10:55:21 +0700

    Add language and effect player routes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import playerRoutes from './routes/playerRoutes';
 import roomRoutes from './routes/roomRoutes';
 import itemRoutes from './routes/itemRoutes';
 import gameRoutes from './routes/gameRoutes';
+import languageRoutes from './routes/languageRoutes';
+import effectPlayerRoutes from './routes/effectPlayerRoutes';
 
 const app = express();
 
@@ -12,4 +14,6 @@ app.use('/api', playerRoutes);
 app.use('/api', roomRoutes); // Assuming you have roomRoutes defined in a similar way
 app.use('/api', itemRoutes);
 app.use('/api', gameRoutes);
+app.use('/api', languageRoutes);
+app.use('/api', effectPlayerRoutes);
 export default app;
