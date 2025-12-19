9896bf15255ccf99102613e473f6e8a79de60534
Quy <57335096+ntquy123@users.noreply.github.com>
2025-12-19 11:00:04 +0700

    Pass session properties via env for server

--- src/services/matchmakingService.ts
+++ src/services/matchmakingService.ts
@@ -111,9 +111,9 @@ function buildContainerName(roomName: string): string {
 
 async function startRoomContainer(roomName: string, port: number, sessionProperties?: string) {
   const containerName = buildContainerName(roomName);
-  const sessionPropertyArg = sessionProperties ? `--sessionProperties="${sessionProperties}"` : '';
+  const sessionPropertyEnv = sessionProperties ? `-e SessionProperties="${sessionProperties}"` : '';
   const startCommand =
-    `${DOCKER_RUNTIME} run -d --rm --name ${containerName} -p ${port}:${SERVER_PORT_IN_CONTAINER} ${DOCKER_IMAGE} ${EXTRA_SERVER_ARGS} --roomName=${roomName} --port=${port} ${sessionPropertyArg}`.trim();
+    `${DOCKER_RUNTIME} run -d --rm --name ${containerName} -p ${port}:${SERVER_PORT_IN_CONTAINER} ${sessionPropertyEnv} ${DOCKER_IMAGE} ${EXTRA_SERVER_ARGS} --roomName=${roomName} --port=${port}`.trim();
 
   const { stderr, stdout } = await execPromise(startCommand);
   const stderrLines = stderr
